99d96cd76bd55f663ea94cc77ccd036f2f0b375c
moetmw <30607334+moetmw@users.noreply.github.com>
2024-12-18 10:57:13 -0500

    tenant_short_name

--- terraform/modules/resource_group/main.tf
+++ terraform/modules/resource_group/main.tf
@@ -1,6 +1,6 @@
 # Create a resource group
 resource "azurerm_resource_group" "rg" {
-  name     = "tn0e-dev-eastus-management"
+  name     = "${var.tenant_shortname}-dev-${var.location}-management"
   location = var.location
 
   tags = {
