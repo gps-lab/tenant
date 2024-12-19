e672d9c3882269ac1ef6a327b254d51dd896c03d
moetmw <30607334+moetmw@users.noreply.github.com>
2024-12-18 19:09:57 -0500

    test

--- terraform/modules/resource_group/main.tf
+++ terraform/modules/resource_group/main.tf
@@ -1,16 +1,3 @@
-# # Create a resource group
-# resource "azurerm_resource_group" "rg" {
-#   name     = "${var.tenant_shortname}-dev-${var.location}-management"
-#   location = var.location
-
-#   tags = {
-#     Environment = "Development"
-#     Project     = "Hello World"
-#     CreatedBy   = "Terraform"
-#     ManagedBy   = "GitHub Actions"
-#   }
-# }
-
 locals {
   environment        = "dev"
   resource_group_name = "${var.tenant_shortname}-${local.environment}-${var.location}-management"
