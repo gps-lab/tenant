ff8d7a6f8e131923012b0a928d41b2319d29ae9d
moetmw <30607334+moetmw@users.noreply.github.com>
2024-12-18 19:50:17 -0500

    add date tag

--- terraform/modules/resource_group/main.tf
+++ terraform/modules/resource_group/main.tf
@@ -1,10 +1,13 @@
 locals {
   environment        = "dev"
   resource_group_name = "${var.tenant_shortname}-${local.environment}-${var.location}-management"
+  current_time       = timestamp()
+  formatted_date     = formatdate("YYYY-MM-DD", local.current_time)
   tags = {
     Environment = var.environment
     CreatedBy   = "Terraform"
     ManagedBy   = "GitHub Actions"
+    CreatedDate = local.formatted_date
   }
 }
 
